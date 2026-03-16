488421e6a49868034640696c0c7084d548c6c0a2
lucky varma <neelamlakshminarayana88@gmail.com>
2026-03-16 17:34:16 +0530

    added

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,14 +22,6 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "${replace(var.app_name, "-", "")}acr"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.app_name}-aks"
   location            = azurerm_resource_group.main.location
@@ -47,9 +39,4 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
+
